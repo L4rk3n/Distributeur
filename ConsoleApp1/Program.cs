497f0282daa0e5f6076863d6a15c1f5caae6254a
Bouvier Cédric <cedric.bounidas@gmail.com>
2024-09-23 16:50:29 +0200

    V2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,59 +1,59 @@
 ﻿using System;
 
-class Program
+class DistributeurDeBoissons
 {
     static void Main()
     {
-        int stock1 = 5; 
-        int stock2 = 2; 
-        int stock3 = 0; 
+        int stock1 = 0;
+        int stock2 = 2;
+        int stock3 = 3;
         int choix;
 
-        Console.WriteLine("Veuillez sélectionner une boisson :");
-        Console.WriteLine("1. Eau");
-        Console.WriteLine("2. Soda");
-        Console.WriteLine("3. Orangeade");
-
-        choix = int.Parse(Console.ReadLine());
-
-        switch (choix)
+        do
         {
-            case 1:
-                if (stock1 != 0)
+            Console.WriteLine("Choisissez une boisson. Boisson1: 1, Boisson2: 2, Boisson3: 3, FINIR : 4");
+            choix = int.Parse(Console.ReadLine());
+
+            if (choix == 1)
+            {
+                if (stock1 > 0)
                 {
-                    Console.WriteLine("Voici votre eau");
+                    Console.WriteLine("Voici votre boisson1, santé!");
                     stock1--;
                 }
                 else
                 {
-                    Console.WriteLine("Sold out!");
+                    Console.WriteLine("Plus de boisson1");
                 }
-                break;
-            case 2:
-                if (stock2 != 0)
+            }
+            else if (choix == 2)
+            {
+                if (stock2 > 0)
                 {
-                    Console.WriteLine("Voici votre soda");
+                    Console.WriteLine("Voici votre boisson2, santé!");
                     stock2--;
                 }
                 else
                 {
-                    Console.WriteLine("Sold out!");
+                    Console.WriteLine("Plus de boisson2");
                 }
-                break;
-            case 3:
-                if (stock3 != 0)
+            }
+            else if (choix == 3)
+            {
+                if (stock3 > 0)
                 {
-                    Console.WriteLine("Voici votre orangeade");
+                    Console.WriteLine("Voici votre boisson3, santé!");
                     stock3--;
                 }
                 else
                 {
-                    Console.WriteLine("Sold out!");
+                    Console.WriteLine("Plus de boisson3");
                 }
-                break;
-            default:
-                Console.WriteLine("Choix indisponible");
-                break;
-        }
+            }
+
+        } while ((stock1 + stock2 + stock3) > 0 && choix != 4);
+
+        Console.WriteLine("Merci d'utiliser distributeur 3000!");
     }
 }
+
